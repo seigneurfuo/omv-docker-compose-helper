4e4c8ab72fcbb7460e357c612158e0f9b00cd3ff
seigneurfuo <seigneurfuo@protonmail.com>
2024-12-13 23:25:07 +0100

    Update omv-docker-compose-helper.py

--- omv-docker-compose-helper.py
+++ omv-docker-compose-helper.py
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Version: 0.1
+# Version: 0.1.1
 
 import argparse
 import os
@@ -21,7 +21,7 @@ def main(args):
         if not os.path.isfile(stack_yml):
             continue
 
-        msg = f"\n[{stack_index+1}/{stacks_count}] {stack_folderpath}"
+        msg = f"\n[{stack_index+1}/{stacks_count}] {stack}: {stack_yml}"
         print(msg)
 
         cmd = f"""/usr/bin/docker compose --file "{stack_folderpath}/{stack}.yml" \
@@ -40,5 +40,9 @@ if __name__ == "__main__":
     argument_parser = argparse.ArgumentParser()
     argument_parser.add_argument("action", help="up | down")
     args = argument_parser.parse_args()
+
+    if args.action not in ["up", "down"]:
+        argument_parser.print_help()
+        exit()
 
     main(args)
