7f9ab377f5850c36d6808c11a9840c5a53549fb7
seigneurfuo <seigneurfuo@protonmail.com>
2024-12-13 23:54:24 +0100

    Update omv-docker-compose-helper.py

--- omv-docker-compose-helper.py
+++ omv-docker-compose-helper.py
@@ -1,48 +1,71 @@
 #!/bin/bash
 
-# Version: 0.1.1
+# Version: 0.2.0
 
 import argparse
+from ast import arg
 import os
 
 STACKS_FOLDERPATH = "/docker"
 
-def main(args):
-    stacks = [
-        stack_folder for stack_folder in os.listdir(STACKS_FOLDERPATH) 
-        if os.path.isdir(os.path.join(STACKS_FOLDERPATH, stack_folder))
-    ]
+def execute_command(stack, action):
+    stack_folderpath = os.path.join(STACKS_FOLDERPATH, stack)
+    stack_yml = os.path.join(stack_folderpath, f"{stack}.yml")
 
-    stacks_count = len(stacks)
-    for stack_index, stack in enumerate(stacks):
-        stack_folderpath = os.path.join(STACKS_FOLDERPATH, stack)
-        stack_yml = os.path.join(stack_folderpath, f"{stack}.yml")
+    if not os.path.isfile(stack_yml):
+        return
 
-        if not os.path.isfile(stack_yml):
-            continue
+    msg = f"Stack path: {stack_yml}"
+    print(msg)
 
-        msg = f"\n[{stack_index+1}/{stacks_count}] {stack}: {stack_yml}"
-        print(msg)
+    cmd = f"""/usr/bin/docker compose --file "{stack_folderpath}/{stack}.yml" \
+        --file "{stack_folderpath}/compose.override.yml" \
+        --env-file "{STACKS_FOLDERPATH}/global.env" \
+        --env-file "{stack_folderpath}/{stack}.env" {action}"""
 
-        cmd = f"""/usr/bin/docker compose --file "{stack_folderpath}/{stack}.yml" \
-            --file "{stack_folderpath}/compose.override.yml" \
-            --env-file "{STACKS_FOLDERPATH}/global.env" \
-            --env-file "{stack_folderpath}/{stack}.env" {args.action}"""
+    # On lance en mode détaché si on lance en up
+    if action == "up":
+        cmd += " -d"
 
-        # On lance en mode détaché si on lance en up
-        if args.action == "up":
-            cmd += " -d"
+    os.system(cmd)
 
-        os.system(cmd)
-
-
-if __name__ == "__main__":
+def main():
     argument_parser = argparse.ArgumentParser()
-    argument_parser.add_argument("action", help="up | down")
+    argument_parser.add_argument("action", help="up | down | list")
+    argument_parser.add_argument("--stack", help="")
     args = argument_parser.parse_args()
 
-    if args.action not in ["up", "down"]:
+    stacks = [
+        stack_folder for stack_folder in os.listdir(STACKS_FOLDERPATH) 
+        if os.path.isdir(os.path.join(STACKS_FOLDERPATH, stack_folder)) and
+        not stack_folder.startswith(".")
+    ]
+
+    # Tri par ordre alphabétique
+    stacks.sort()
+
+    if args.action == "list":
+        msg = " ".join(stacks)
+        print(msg)
+        exit()
+
+    elif args.action not in ["up", "down"]:
         argument_parser.print_help()
         exit()
 
-    main(args)
+    # Si on défini une stack unique
+    if args.stack:
+        execute_command(args.stack, args.action)
+        exit()
+
+
+    stacks_count = len(stacks)
+    for stack_index, stack in enumerate(stacks):
+        msg = f"\n[{stack_index+1}/{stacks_count}] {stack}"
+        print(msg)
+
+        execute_command(stack, args.action)
+
+
+if __name__ == "__main__":
+    main()
